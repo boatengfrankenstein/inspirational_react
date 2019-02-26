4240baf6cfa184589c83bd712d2921c93018a969
daniel <boatengfrankenstein@gmail.com>
2019-02-26 13:16:55 -0500

    webpack server

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root to: "pages#home"
+  namespace :api, defaults: { format: :json } do
+    resources :quotes, only: [ :show ]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Quote.delete_all
+
+Quote.create! (
+  [
+    {
+      text: "Be yourself; everyone else is already taken.",
+      author: "Oscar Wilde"
+    },
+    {
+      text: "Two things are infinite: the universe and human stupidity; " \
+            "and I'm not sure about the universe.",
+      author: "Albert Einstein"
+    },
+    {
+      text: "So many books, so little time.",
+      author: "Frank Zappa"
+    },
+    {
+      text: "Be the change that you wish to see in the world",
+      author: "Mahatma Gandhi"
+    },
+    {
+      text: "If you tell the truth, you don't have to remember anything.",
+      author: "Mark Twain"
+    }
+  ]
+)
+puts "Quotes seeded!"
